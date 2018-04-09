c44ee5a70c47d70ab298d5925e0765d3a8fc08b0
iamsanketsingh <iamsanketsingh1998@gmail.com>
2018-04-09 21:58:32 +0530

    Comments Added

--- stack new.cpp
+++ stack new.cpp
@@ -1,7 +1,8 @@
+// creating an stack using dynamic array
 #include<stdio.h>
 #include<stdlib.h>
 struct stack  {
-  private:
+  private:              // declared two integer type pointers and two integers named as max and count
    int *stk;
    int *top;
     int max;
@@ -13,42 +14,42 @@ struct stack  {
     
    	printf("enter the value of number of elements ");
    	scanf("%d",&max);
-   	stk=(int*)malloc(sizeof(int)*max);
+   	stk=(int*)malloc(sizeof(int)*max);       // creating the dynamic array, followed by push and pop operations 
    	top=stk;
    	
    }
-   void push(int val)
+   void push(int val)                        // push operation to insert the new value in stack
    {
-   	count++;
-    if(count>max-1)
+   	count++;                                 // we are incrementing the count variable at the start of this method because we started with -1 
+    if(count>max-1)                          // first of all we will go for the condition of overflow
 	{
-		printf("\n stack overflow");
+		printf("\n stack overflow");        
 	}  	
 	else
 	{
-		top++;
-		stk[count]=val;
+		top++;                               
+		stk[count]=val;                        // incrementing the value of top and pushing new values in stack
 		
 	}
 	
    }
    
-   void pop()
+   void pop()                                 // pop operation to delete the new value from stack
    { 
      
    	if(count==-1)
    	{ 
-	   printf("\n stack underflow");
+	   printf("\n stack underflow");         // first of all we will go for the condition of underflow
    	}
    	else
    	{
-   		printf("\n the deleted element from stack is %d",stk[count]);
+   		printf("\n the deleted element from stack is %d",stk[count]);       //   decrementing the value of top and poping the  new values out of stack
    		top--;
    	}
     count--;
    }
    
-   void display()
+   void display()                            // normal display function used to display the stack
    {
    	if(count==-1)
    	{
@@ -62,7 +63,7 @@ struct stack  {
    }
    
     }s1,s2;
-  int main()
+  int main()                       // main fuctions where we are calling these methods
   {  
     s1.extra();
   	s1.push(12);
